8057302da2a478c20b70d43811ba3fa1e80b2909
Seongmin Cheon <seongmin.cheon21@gmail.com>
2026-06-02 08:51:42 +0900

    update
    update

---
+++ config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec",
+  REFRESH_INTERVAL_MS: 0,  // 0 = 수동 새로고침만, 양수 = 자동 갱신 주기(ms)
+  USE_MOCK_DATA: true       // 로컬 테스트 시 true, 실제 배포 시 false
+};
