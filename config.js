c98ee563f7f50d2118070c680757c22d5995f2e7
Seongmin Cheon <seongmin.cheon21@gmail.com>
2026-06-02 14:35:14 +0900

    update code
    update code

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxjyIhZVAkcu-Vb6Zy4eyFiATGDaiy793u_x1ygf_-aLXd6OJB-ECX0g7tjsdrv6S-5Lg/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbxngKzKEOSFmE_OMAEXCd1BPaHpF0uJSFzeU5N5HN-cH73-aZfCcGzWHxMr9Blqtro-7A/exec",
   REFRESH_INTERVAL_MS: 0,  // 0 = 수동 새로고침만, 양수 = 자동 갱신 주기(ms)
   USE_MOCK_DATA: true   // ← 로컬 확인용 (배포 전 false로 변경)       // 로컬 테스트 시 true, 실제 배포 시 false
 };
